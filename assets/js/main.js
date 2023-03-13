27196f8e366d08c8aae7532d6d716a26d249c3d1
Alex Rodrigues <alexmusicalsa@gmail.com>
2023-03-13 20:59:56 -0300

    segmento de estilo

---
+++ assets/js/main.js
@@ -0,0 +1,7 @@
+var typed = new Typed(".multiple-text", {
+    strings: ["Analista de Sistemas", "Desenvolvedor Frontend"],
+    typeSpeed: 100,
+    backSpeed: 100,
+    backDelay: 1000,
+    loop: true
+});
