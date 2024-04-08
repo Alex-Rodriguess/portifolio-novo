c3c66fd44aecd135a38a1b95b7642cf0d48bb7ba
Alex Rodrigues <alexmusicalsa@gmail.com>
2024-04-08 10:25:24 -0300

    mudanças nas imagens e no conteudo

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,5 +1,5 @@
 var typed = new Typed(".multiple-text", {
-    strings: ["Analista de Sistemas", "Dev Front-end"],
+    strings: ["Técnico de TI", "Analista de Sistemas", "Desenvolvedor Web"],
     typeSpeed: 100,
     backSpeed: 100,
     backDelay: 1000,
